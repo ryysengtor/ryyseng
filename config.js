0e53de9e4b80a8aede5dd7f81f5cc4315c9bb597
ryysengtor <emulatorgamehub3@gmail.com>
2026-04-24 18:50:33 +0700

    Initial commit: config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+  STORE_NAME: 'Ryysengtor',
+
+  STORE_DESCRIPTION: 'Panel hosting, API, OTP, dan berbagai produk digital premium dengan harga terjangkau dan proses otomatis.',
+
+  STORE_WA: '',
+
+  STORE_CHANNEL: '',
+
+  STORE_INSTAGRAM: '',
+
+  STORE_TIKTOK: '',
+
+  STORE_LOGO: '/media/logo.jpg',
+
+  STORE_ICON: '/media/icon.jpg',
+
+  STORE_PRIMARY_COLOR: '#34d399',
+
+  STORE_EXPIRY_MIN: 15,
+
+};
